78b5b181d1e10d1d61d4bcb34093642efdc72c71
koftov <ahronlu@gmail.com>
2020-07-07 23:51:28 +0300

    Updates

---
+++ precache-manifest.ec3a2ee8a5794c2e15c4e31916e5d8cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000aa843fcb621abb085eacd118e5b75",
+    "url": "/dad-jokes-api/index.html"
+  },
+  {
+    "revision": "a94fb667469749dd596f",
+    "url": "/dad-jokes-api/static/css/main.7dacc3e6.chunk.css"
+  },
+  {
+    "revision": "dc8ec4884526cd1780f4",
+    "url": "/dad-jokes-api/static/js/2.65f73110.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes-api/static/js/2.65f73110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a94fb667469749dd596f",
+    "url": "/dad-jokes-api/static/js/main.a20642d8.chunk.js"
+  },
+  {
+    "revision": "5e5f5c052f2bf82d6865",
+    "url": "/dad-jokes-api/static/js/runtime-main.3efae4f8.js"
+  }
+]);
